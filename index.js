2fe9e6bd6e26f5455ff6fdb271532cb16f543642
matth3wn <matth3wn@gmail.com>
2019-01-18 15:06:35 -0800

    starting edit feature

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const STORE = {
 function generateItemElement(item, itemIndex, template) {
   return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
-      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
-      <span class= "shopping-item js-shopping-edit hidden><input value ="${item.name}"></input><button class='edit-button'>Ok</button></span>
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <span class="js-shopping-item-edit hidden"><input value=${item.name}></input><button class="edit-button">OK</button></span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
@@ -137,13 +137,20 @@ function editListItem(itemIndex,replace){
 
 
 //handles edit on the OKAY 
-function handlesEdit(){
-  $('#js-shopping-list-form').on('click', '.js-shopping item', function(event){
-    $(event.target).closest('li').find('.shopping-item js-shopping-edit hidden').toggleClass('hidden');
-    $(event.target).closest('li').find('.shopping-item js-shopping hidden').toggleClass('hidden');
-    const itemIndex = getItemIndexFromElement(event.currentTarget);
-    console.log($(this).text());
+function handleEditItemClicked() {
+  // we attach the event listener on the element
+  $('.js-shopping-list').on('click', '.js-shopping-item', event => {
+
+    // look for the current/clicked-on item and find the input and remove hidden
+    $(event.target).closest('li').find('.js-shopping-item-edit').toggleClass('hidden');
+    // look for the current item and find the `item` and hide it
+    $(event.target).closest('li').find('.js-shopping-item').toggleClass('hidden');
+
   });
+}
+
+function saveChange(index){
+  
 }
 // need event listener for the double click <span> and to show the hidden 
 
@@ -153,7 +160,7 @@ function handleShoppingList() {
   handleItemCheckClicked();
   handleDeleteItemClicked();
   handleToggleCheck();
-  handlesEdit();
+  handleEditItemClicked();
 }
 
 // when the page loads, call `handleShoppingList`
